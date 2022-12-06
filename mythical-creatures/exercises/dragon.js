4ea99594ec3a5732b5d0260aa44985b952a71a06
Kirk Hauck <kirkihauck@gmail.com>
2022-12-06 16:38:08 -0700

    Add updated README.md and completed dragon.js and dragon-test.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,25 @@
+class Dragon {
+    
+    constructor (name, rider, hungry) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = hungry || true;
+        this.eatCount = 0;
+    }
 
+    greet(rider) {
+        return (`Hi, ${this.rider}!`);
+    }
+
+    eat() {
+        this.eatCount += 1;
+        
+        if(this.eatCount >= 3) {
+            this.hungry = false;
+        }
+    }
+}
+
+
+
+module.exports = Dragon;
