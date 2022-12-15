d72f289e5e3823d91393bbced1b3cf4fd12d4871
Kirk Hauck <kirkihauck@gmail.com>
2022-12-15 16:45:07 -0700

    Add completed gear in oregon-trail

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,18 @@
 class Gear {
+  constructor(gearType, gearQuantity) {
+    this.type = gearType;
+    this.quantity = gearQuantity;
+  }
 
+  checkForValidity() {
+    if (this.type === 'ammunition' || this.type === 'clothes' || this.type === 'food') {
+      return 'Great, we\'ll need lots of ammunition!';
+    } else {
+      var message = `I don\'t think a ${this.type} will help us.`
+      this.type = null;
+      return message;
+    }
+  }
 }
 
 module.exports = Gear;
