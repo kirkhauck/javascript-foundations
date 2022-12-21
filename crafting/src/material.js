47e31b664bde0cbed9f3e2a513cd6663cdea503f
Kirk Hauck <kirkihauck@gmail.com>
2022-12-20 18:22:53 -0700

    Add completed materials in crafting

--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,23 @@
 class Material {
+  constructor(matName, matPrice, matAmount, matUnits) {
+    this.name = matName;
+    this.price = matPrice;
+    this.amount = matAmount;
+    this.units = matUnits;
+  }
 
+  useMaterial(num) {
+    if (this.amount >= num) {
+      this.amount -= num;
+      return `You now have ${this.amount} ${this.units} of ${this.name} left.`;
+    } else {
+      return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`;
+    }
+  }
+
+  calculateMaterialCost() {
+    return this.price * this.amount;
+  }
 }
 
 module.exports = Material;
