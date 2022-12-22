b2893763047c9fe7729d209b5319ea797a609c50
Kirk Hauck <kirkihauck@gmail.com>
2022-12-21 17:10:43 -0700

    Add completed golf-course

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,23 @@
 class GolfCourse {
+  constructor(courseName, courseDifficulty, courseOpenings, courseFeatures) {
+    this.name = courseName;
+    this.difficulty = courseDifficulty;
+    this.openings = courseOpenings
+    this.features = courseFeatures
+    this.currentlyPlaying = [];
+  }
 
-
+  checkInGroup(group) {
+    if (group.length <= this.openings) {
+      this.openings = this.openings - group.length;
+      for (var i = 0; i < group.length; i++) {
+        this.currentlyPlaying.unshift(group[i].name);
+      }
+      return 'You\'re checked in. Have fun!';
+    } else {
+      return 'Sorry, we are currently booked! Please come back later.';
+    }
+  }
 }
 
 module.exports = GolfCourse;
