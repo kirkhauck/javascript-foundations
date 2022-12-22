23ddc8dfd32a97338149177a50d166a0b7070700
Kirk Hauck <kirkihauck@gmail.com>
2022-12-21 17:41:23 -0700

    Add completed fairy

---
+++ mythical-creatures/exercises/fairy.js
@@ -0,0 +1,41 @@
+class Fairy {
+  constructor(fairyName) {
+    this.name = fairyName;
+    this.dust = 10;
+    this.clothes = {dresses: ['Iris']};
+    this.disposition = 'Good natured';
+    this.humanWards = [];
+  }
+
+  receiveBelief() {
+    this.dust += 1;
+  }
+
+  believe() {
+    this.dust += 10;
+  }
+
+  makeDresses(flowers) {
+    for (var i = 0; i < flowers.length; i++) {
+      this.clothes.dresses.push(flowers[i]);
+    }
+  }
+
+  becomeProvoked() {
+    this.disposition = 'Vengeful';
+  }
+
+  replaceInfant(infant) {
+    if (this.disposition === 'Vengeful') {
+      infant.disposition = 'Malicious';
+      this.humanWards.push(infant);
+    } else {
+      return infant;
+    }
+    if (this.humanWards.length >= 3) {
+      this.disposition = 'Good natured';
+    }
+  }
+}
+
+module.exports = Fairy;
