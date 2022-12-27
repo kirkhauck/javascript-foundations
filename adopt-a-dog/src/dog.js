6ffd29944991c7292e9e42a8f9dedc3f790840a7
Kirk Hauck <kirkihauck@gmail.com>
2022-12-27 15:49:32 -0700

    Add completed dog and dog test for adopt a dog

--- adopt-a-dog/src/dog.js
+++ adopt-a-dog/src/dog.js
@@ -1,8 +1,41 @@
 class Dog {
-  constructor() {
-
+  constructor(dog) {
+    this.name = dog.name;
+    this.age = dog.age;
+    this.energyLevel = 5;
+    this.hungry = true;
+    this.friends = [];
   }
 
+  eat() {
+    if (this.hungry) {
+      this.hungry = false;
+      return 'Yum!';
+    } else {
+      return 'I refuse to eat.';
+    }
+  }
+
+  fetchBall() {
+    if (this.energyLevel > 3) {
+      this.energyLevel -= 1;
+      return 'This is fun!';
+    } else {
+      return 'Nah, I\'m going to sleep instead.';
+    }
+  }
+
+  sleep() {
+    if (this.energyLevel < 10) {
+      this.energyLevel += 1;
+    } else {
+      return 'I have too much energy to rest. I\'m going to chew something instead.';
+    }
+  }
+
+  makeNewFriend(newFriend) {
+    this.friends.push(newFriend.name);
+  }
 }
 
 module.exports = Dog;
