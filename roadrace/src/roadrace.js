b9aca70d045a1bf386f452db42a396acd5270cef
Kirk Hauck <kirkihauck@gmail.com>
2023-01-02 18:38:23 -0700

    Add completed roadrace

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,25 @@
 class Roadrace {
+  constructor(race) {
+    this.name = race.title;
+    this.location = race.city;
+    this.distance = 0;
+    this.participants = [];
+  }
 
+  setDistance(miles) {
+    this.distance = miles;
+    return `The ${this.name} in ${this.location} is a ${miles} mile race.`;
+  }
+
+  registerParticipants(participant) {
+    this.participants.push(participant);
+  }
+
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++) {
+      this.participants[i].runRace(this.name, this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,25 @@
 class Runner {
+  constructor(runnerName, runnerAge) {
+    this.name = runnerName;
+    this.age = runnerAge;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
 
+  runSomeMiles(miles) {
+    this.milesRun += miles;
+    this.fitness += miles;
+  }
+
+  stretch() {
+    this.fitness += 0.5;
+  }
+
+  runRace(name, miles) {
+    this.completedRaces.push(name);
+    this.runSomeMiles(miles);
+  }
 }
 
 module.exports = Runner;
