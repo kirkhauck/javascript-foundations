4fb01d68c556b6c443ae7161f783a8fdbe8105a8
Kirk Hauck <kirkihauck@gmail.com>
2023-01-05 19:02:55 -0700

    Add completed recipes

--- recipes/src/chef.js
+++ recipes/src/chef.js
@@ -1,7 +1,33 @@
 var Recipe = require('../src/recipe.js');
 
 class Chef {
-  
+  constructor(name, recipes) {
+    this.name = name;
+    this.recipeBox = recipes;
+  }
+
+  tryRecipe(recipe, rating) {
+    for (var i = 0; i < this.recipeBox.length; i++) {
+      if (this.recipeBox[i].name = recipe) {
+        this.recipeBox[i].attempted = true;
+        this.recipeBox[i].rating = rating;
+      }
+    }
+  }
+
+  addRecipe(recipe, ingredients) {
+    var newRecipe = {name: recipe, ingredients};
+    newRecipe = new Recipe(newRecipe);
+    this.recipeBox.push(newRecipe);
+  }
+
+  changeRecipe(recipe, ingredient, amount) {
+    for (var i = 0; i < this.recipeBox.length; i++) {
+      if (recipe === this.recipeBox[i].name) {
+        this.recipeBox[i].changeIngredientAmount(ingredient, amount);
+      }
+    }
+  }
 }
 
 module.exports = Chef;
--- recipes/src/recipe.js
+++ recipes/src/recipe.js
@@ -1,5 +1,31 @@
 class Recipe {
+  constructor(recipe) {
+    this.name = recipe.name;
+    this.ingredients = recipe.ingredients;
+    this.attempted = false;
+    this.rating = null;
+  }
 
+  rateRecipe(rating) {
+    this.attempted = true;
+    this.rating = rating;
+  }
+
+  changeIngredientAmount(ingredient, amount) {
+    for (var i = 0; i < this.ingredients.length; i++) {
+      if (this.ingredients[i].name = ingredient) {
+        this.ingredients[i].amount = amount;
+      }
+    }
+  }
+
+  generateGroceryList() {
+    var groceryList = 'You need:';
+    for (var i = 0; i < this.ingredients.length; i++) {
+      groceryList += ` ${this.ingredients[i].amount} ${this.ingredients[i].name},`;
+    }
+    return groceryList;
+  }
 }
 
 module.exports = Recipe;
