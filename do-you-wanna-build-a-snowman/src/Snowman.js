fd7b8ac6b2e272cd99d570e7df5dc46c708e6776
Kirk Hauck <kirkihauck@gmail.com>
2023-01-09 13:11:44 -0700

    Add completed snowman

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,20 @@
 class Snowman {
+  constructor(snowman) {
+    this.carrots = snowman.carrots;
+    this.coal = snowman.coal;
+    this.buttons = snowman.buttons;
+    this.snowballs = snowman.snowballs;
+    this.magicHat = false;
+  }
 
+  canWearMagicHat() {
+    if (this.coal < 2 || this.buttons < 5 || this.carrots < 1 || this.snowballs < 2) {
+      return false;
+    } else {
+      this.magicHat = true;
+      return true;
+    }
+  }
 }
 
 module.exports = Snowman;
